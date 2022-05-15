2b901692286ed0e7d15a18335db9f904a6e2a122
jsmith2589 <65322603+jsmith2589@users.noreply.github.com>
2022-05-14 23:56:02 -0400

    completed codewars setup

---
+++ js/solution001.js
@@ -0,0 +1,6 @@
+//8KYU L1:Set Alarm
+// are you employeed? Are you on vacation? Return answer/ do you need an alarm
+
+function setAlarm(employed, vacation) {
+  return employed == true && vacation == false ? true : false
+}
